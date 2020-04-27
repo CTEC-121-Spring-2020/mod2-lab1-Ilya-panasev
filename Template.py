5aba58e59a26a3ef47a4cd438c8a43304d398e03
Ilya <i.panasevich@students.clark.edu>
2020-04-27 12:25:47 -0700

    Module 2 Lab 1 practice in class

--- Template.py
+++ Template.py
@@ -1,8 +1,8 @@
 """
 CTEC 121
-<your name>
-<assignment/lab name>
-<assignment/lab description
+Ilya Panasevich
+Module 2 lab 1
+Class demos
 """
 
 """ IPO template
@@ -12,6 +12,45 @@ Output: return value and description
 """
 
 def main():
-    # code here
+    '''
+    A = 2
+    print("\n", A, "\n")
+    a = 22
+    print(a)
+    print()
 
+    AAA = 2
+    print(AAA)
+    
+    spam = 222
+    print()
+    print(spam)
+    #print(Spam)
+    print()
+    
+    # expression
+    #print(2)
+    print(3 - 1)
+    print((3 - 1) / 2)
+    print(5/3)
+    print(5//3)
+    print(5%3)
+    print(2 + ((3 - 1) / 2))
+    
+    print()
+    print("the answer is:", 3 + 4, sep="-", end="\t")
+    print("end")
+    print()
+
+    for i in range(5):
+        print(i, end="\t")
+    print()
+    '''
+    # exercise
+    ClassCode = "CTEC 121"
+    ClassCount = 18
+    ClassName = "Introduction to Programming and Problem Solving"
+    # Welcome to <ClassCode>. The student count is <ClassCount> and the name of the class is <ClassName>
+    print("Welcome to ", ClassCode, ". The student count is ", ClassCount, " and the name of the class is ", ClassName, ".", sep="")
+    print()
 main()    
